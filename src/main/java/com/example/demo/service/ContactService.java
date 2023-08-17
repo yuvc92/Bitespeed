31c1cc630a5478eea057622b223605364774c212
yuvc92 <61701836+yuvc92@users.noreply.github.com>
2023-08-17 22:09:15 +0530

    DB_configured

--- src/main/java/com/example/demo/service/ContactService.java
+++ src/main/java/com/example/demo/service/ContactService.java
@@ -1,11 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.DTO.Contact;
+import com.example.demo.DTO.ContactRequest;
+import com.example.demo.DTO.ContactResponse;
 import com.example.demo.model.CustomerContact;
 import com.example.demo.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ContactService {
@@ -30,5 +35,85 @@ public class ContactService {
         newContact.setLinkPrecedence(precedence);
         newContact.setLinkedId(linkedId);
         contactRepository.save(newContact);
+    }
+
+
+    public ContactResponse identify(@RequestBody ContactRequest contactRequest) {
+        ContactResponse response = new ContactResponse();
+        List<CustomerContact> matchingEmail = contactRepository.findAllByEmail(contactRequest.getEmail());
+        List<CustomerContact> matchingPhone = contactRepository.findAllByPhoneNumber(contactRequest.getPhoneNumber());
+        if(matchingEmail.isEmpty() && matchingPhone.isEmpty()){
+            Long newId = this.createNewContact(
+                    contactRequest.getEmail(),
+                    contactRequest.getPhoneNumber(),
+                    "primary"
+            );
+            Contact contact = new Contact();
+            contact.setPrimaryContactId(newId);
+            contact.setEmails(List.of(contactRequest.getEmail()));
+            contact.setPhoneNumbers(List.of(contactRequest.getPhoneNumber()));
+            response.setContact(contact);
+        }
+        else if(matchingEmail.isEmpty()){
+            Long primaryId = matchingPhone.get(0).getLinkedId();
+            this.createNewContact(
+                    contactRequest.getEmail(),
+                    contactRequest.getPhoneNumber(),
+                    primaryId,
+                    "secondary"
+            );
+            List<CustomerContact>contactList = contactRepository.findAllByLinkedId(primaryId);
+            List<Integer>phoneList = contactList.stream().map(CustomerContact::getPhoneNumber).distinct().toList();
+            List<String>emailList = contactList.stream().map(CustomerContact::getEmail).distinct().toList();
+            List<Long>secondaryContactList = contactList.stream().map(CustomerContact::getId).filter(id -> !id.equals(primaryId)).toList();
+            Contact contact = new Contact();
+            contact.setPrimaryContactId(primaryId);
+            contact.setEmails(emailList);
+            contact.setPhoneNumbers(phoneList);
+            contact.setSecondaryContactIds(secondaryContactList);
+            response.setContact(contact);
+        }
+        else if(matchingPhone.isEmpty()){
+            Long primaryId = matchingEmail.get(0).getLinkedId();
+            this.createNewContact(
+                    contactRequest.getEmail(),
+                    contactRequest.getPhoneNumber(),
+                    primaryId,
+                    "secondary"
+            );
+            List<CustomerContact>contactList = contactRepository.findAllByLinkedId(primaryId);
+            List<String>emailList = contactList.stream().map(CustomerContact::getEmail).distinct().toList();
+            List<Integer>phoneList = contactList.stream().map(CustomerContact::getPhoneNumber).distinct().toList();
+            List<Long>secondaryContactList = contactList.stream().map(CustomerContact::getId).filter(id -> !id.equals(primaryId)).toList();
+            Contact contact = new Contact();
+            contact.setPrimaryContactId(primaryId);
+            contact.setEmails(emailList);
+//            contact.setPhoneNumbers(List.of(contactRequest.getPhoneNumber()));
+            contact.setPhoneNumbers(phoneList);
+            contact.setSecondaryContactIds(secondaryContactList);
+            response.setContact(contact);
+        }
+        else{
+            Long primaryId = matchingEmail.get(0).getLinkedId();
+            Long secondaryId = matchingPhone.get(0).getLinkedId();
+            if( !secondaryId.equals(  primaryId ) ){
+                contactRepository.findAllByLinkedId(secondaryId).forEach(contact -> {
+                    contact.setLinkedId(primaryId);
+                    if(contact.getLinkPrecedence().equals("primary"))contact.setLinkPrecedence("secondary");
+                    contactRepository.save(contact);
+                });
+            }
+            List<CustomerContact>contactList = contactRepository.findAllByLinkedId(primaryId);
+            List<String>emailList = contactList.stream().map(CustomerContact::getEmail).distinct().toList();
+            List<Integer>phoneList = contactList.stream().map(CustomerContact::getPhoneNumber).distinct().toList();
+            List<Long>secondaryContactList = contactList.stream().map(CustomerContact::getId).filter(id -> !id.equals(primaryId)).toList();
+            Contact contact = new Contact();
+            contact.setPrimaryContactId(primaryId);
+            contact.setPhoneNumbers(phoneList);
+            contact.setEmails(emailList);
+            contact.setSecondaryContactIds(secondaryContactList);
+            response.setContact(contact);
+        }
+        return response;
     }
 }
