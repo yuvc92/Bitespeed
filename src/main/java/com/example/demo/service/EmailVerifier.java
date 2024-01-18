9a0b2de611ce4466668bb23a9cb7fbef0327052d
yuvc92 <61701836+yuvc92@users.noreply.github.com>
2024-01-19 02:01:05 +0530

    commit1, branch1

--- src/main/java/com/example/demo/service/EmailVerifier.java
+++ src/main/java/com/example/demo/service/EmailVerifier.java
@@ -9,6 +9,6 @@ public class EmailVerifier {
 
     public static boolean validate(String emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
-        return matcher.matches();
+        return matcher.matches(); //commit1
     }
 }
